bf6a36df7ba7e0303e07f2c9f420d7026b01d393
carlosgcazun <quiqueguerra@hotmail.es>
2020-02-29 20:17:28 -0600

    Updates

---
+++ precache-manifest.ef6702c5172d11dd1274f809347fcf36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c352155f6bd04c52c2b02025f1a5b8",
+    "url": "/Rolodex/index.html"
+  },
+  {
+    "revision": "ea088fc4207d1be01100",
+    "url": "/Rolodex/static/css/main.40e589b6.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/Rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea088fc4207d1be01100",
+    "url": "/Rolodex/static/js/main.2cadc5d7.chunk.js"
+  },
+  {
+    "revision": "79328e91142a59474e7a",
+    "url": "/Rolodex/static/js/runtime-main.7351ab76.js"
+  }
+]);
